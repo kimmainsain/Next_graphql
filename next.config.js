e3069dea96a45759aabd0b06b5c77ccdec3fa03a
kim.mainsain <kmsid003@gmail.com>
2024-05-24 00:56:11 +0900

    :heavy_plus_sign: Feat : 로그인 안되어있을경우 로그인 화면으로 라우팅

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const prefix = process.env.NODE_ENV === 'production' ? 'https://kimmainsain.github.io/Solotrip/' : ''
 
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: "export",
   assetPrefix: prefix,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
+"use client";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { authState } from "@/recoils/authState";
+import { useRouter } from "next/navigation";
 
 const Home = () => {
+  const setAuthState = useSetRecoilState(authState);
+  const isLogin = useRecoilValue(authState);
+  const router = useRouter();
+
+  const updateAuthState = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuthState(true);
+    } else {
+      setAuthState(false);
+    }
+  };
+
+  useEffect(() => {
+    updateAuthState();
+  }, []);
+
+  useEffect(() => {
+    if (!isLogin) {
+      router.push("/users/login");
+    }
+  }, [isLogin, router]);
+
   return (
     <div>
       <Link href="/solplaces/create">linkTest</Link>
