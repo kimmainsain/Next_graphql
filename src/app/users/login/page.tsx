36d46c5ba8e0a98905fc4171ab9b7495044334df
kim.mainsain <kmsid003@gmail.com>
2024-05-24 01:35:25 +0900

    :heavy_plus_sign: Feat : 로그인 최소기능구현 완료 / todo 1. graphql 학습 (jwt관련) 2. layout.tsx provider를 하나 분리해버리기 3. graphql 분리

--- src/app/users/login/page.tsx
+++ src/app/users/login/page.tsx
@@ -1,8 +1,53 @@
+"use client";
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+
+const LOGIN_MUTATION = gql`
+  mutation Login($loginInput: LoginInput!) {
+    login(loginInput: $loginInput) {
+      accessToken
+      name
+    }
+  }
+`;
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login] = useMutation(LOGIN_MUTATION);
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await login({
+        mutation: LOGIN_MUTATION,
+        variables: {
+          loginInput: {
+            email,
+            password,
+          },
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 };
