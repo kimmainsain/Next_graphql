dae098b83ce0002c8a88a32f320294650474708e
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-05-23 23:09:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prefix = process.env.NODE_ENV === 'production' ? 'https://kimmainsain.github.io/Solotrip/' : ''
+
 const nextConfig = {
   output: "export",
+  assetPrefix: prefix,
 }
 
 module.exports = nextConfig
