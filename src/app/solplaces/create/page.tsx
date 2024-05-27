fe29876ec2f1f8e5f0b64dc8f0333450fcdfa94e
kim.mainsain <kmsid003@gmail.com>
2024-05-27 21:32:01 +0900

    :art: Design : 사진카드 컴포넌트 1차 퍼블리싱 1. 모듈화 더하기 2. props넘기기

--- src/app/solplaces/create/page.tsx
+++ src/app/solplaces/create/page.tsx
@@ -1,7 +1,15 @@
+import PictureCard from "@/components/solplace/Log/PictureCard";
+
 const CreatePage = () => {
   return (
-    <div>
-      <h1>Create a new place</h1>
+    <div className="w-full p-4">
+      <div className="flex items-center mb-2">
+        <span className="text-lg font-semibold">솔플레이스 사진</span>
+        <span className="ml-2 text-sm text-gray-500">
+          {5}/{5}
+        </span>
+      </div>
+      <PictureCard />
     </div>
   );
 };
--- src/components/solplace/Log/PictureCard.tsx
+++ src/components/solplace/Log/PictureCard.tsx
@@ -1,5 +1,53 @@
+"use client";
+import { useState } from "react";
+import { MAX_SOLPLACE_LOG_PICTURES } from "@/constants/solplaceLog";
+
 const PictureCard = () => {
-  return <div>Picture Card</div>;
+  const [pictures, setPictures] = useState<string[]>([
+    "dummy",
+    "dummy2",
+    "dummy3",
+    "dummy4",
+  ]);
+
+  const handleAddPicture = () => {
+    console.log("Add Picture");
+  };
+
+  const handleRemovePicture = (index: number) => {
+    console.log("Remove Picture", index);
+  };
+
+  return (
+    <div className="flex overflow-x-auto space-x-2">
+      <div
+        className="w-24 h-24 border-2 rounded-md flex flex-col justify-center items-center cursor-pointer flex-shrink-0"
+        onClick={handleAddPicture}
+      >
+        <div className="text-4xl">+</div>
+        <div className="text-center">사진 등록</div>
+      </div>
+      {pictures.map((picture, index) => (
+        <div
+          key={index}
+          className="relative w-24 h-24 flex-shrink-0 bg-red-500 rounded-md"
+        >
+          <img src={picture} alt={`Picture ${index + 1}`} />
+          {index === 0 && (
+            <div className="absolute top-1 left-1 bg-white text-black text-xs px-1 rounded">
+              대표
+            </div>
+          )}
+          <button
+            className="absolute top-1 right-1 bg-white rounded-full cursor-pointer"
+            onClick={() => handleRemovePicture(index)}
+          >
+            <div className="text-xs">X</div>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PictureCard;
