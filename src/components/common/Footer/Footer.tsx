8e6dc94c51baca1907cafb3391363eb2b9fbc6fa
kim.mainsain <kmsid003@gmail.com>
2024-05-28 17:03:48 +0900

    :heavy_plus_sign: Feat : footer 현재 네비게이션이 선택되어 있는 화면 구성

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import unselectedHome from "@/assets/png/Footer/Home.unselected.png";
 import selectedHome from "@/assets/png/Footer/Home.selected.png";
 import unselectedSol from "@/assets/png/Footer/Solplace.unselected.png";
+import selectedSol from "@/assets/png/Footer/Solplace.selected.png";
 
 type FooterMapType = {
   [key: string]: React.ComponentType;
@@ -26,21 +27,48 @@ const Footer = () => {
 };
 
 const MainFooter = () => {
+  const pathname = usePathname();
+  const activePaths = {
+    home: ["/", "/users/main"],
+    solplaces: ["/solplaces/list"],
+  };
+  const isActive = (paths: string[]) => paths.includes(pathname);
+
   return (
     <div className="flex justify-around items-center p-4 h-12 bg-white shadow-md fixed bottom-0 w-full">
       <div className="flex flex-col items-center">
         <Link href="/">
           <div className="flex flex-col items-center">
-            <Image src={unselectedHome} alt="unselectedHome" />
-            <div className="text-xs">홈</div>
+            <Image
+              src={isActive(activePaths.home) ? selectedHome : unselectedHome}
+              alt="Home"
+            />
+            <div
+              className={`text-xs ${
+                isActive(activePaths.home) ? "text-blue-500" : ""
+              }`}
+            >
+              홈
+            </div>
           </div>
         </Link>
       </div>
       <div className="flex flex-col items-center">
         <Link href="/solplaces/list">
           <div className="flex flex-col items-center">
-            <Image src={unselectedSol} alt="unselectedSearch" />
-            <div className="text-xs">솔플레이스</div>
+            <Image
+              src={
+                isActive(activePaths.solplaces) ? selectedSol : unselectedSol
+              }
+              alt="Solplaces"
+            />
+            <div
+              className={`text-xs ${
+                isActive(activePaths.solplaces) ? "text-blue-500" : ""
+              }`}
+            >
+              솔플레이스
+            </div>
           </div>
         </Link>
       </div>
